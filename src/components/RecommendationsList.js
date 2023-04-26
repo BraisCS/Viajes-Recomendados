9782245842278556f3c64b63262ea09cc03d3d14
Alicia <suarezamc@hotmail.com>
2023-04-26 17:31:33 +0200

    list

--- src/components/RecommendationsList.js
+++ src/components/RecommendationsList.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./RecommendationList.css";
+import "./RecommendationsList.css";
 
 export const RecommendationsList = ({ recommendations }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [placeQuery, setPlaceQuery] = useState("");
   const [categoryQuery, setCategoryQuery] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [sortOrder, setSortOrder] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const RECOMMENDATIONS_PER_PAGE = 5;
 
   const handleSearch = () => {
     setSearchQuery(placeQuery.toLowerCase() + categoryQuery.toLowerCase());
@@ -15,6 +19,27 @@ export const RecommendationsList = ({ recommendations }) => {
     setSearchQuery("");
     setPlaceQuery("");
     setCategoryQuery("");
+  };
+
+  const handleSortBy = (sortBy) => {
+    if (sortBy === "toggleSortOrder") {
+      setSortOrder(!sortOrder);
+    } else if (sortBy === "maxmedia") {
+      setSortBy("maxmedia");
+      setSortOrder(true);
+    } else if (sortBy === "minmedia") {
+      setSortBy("minmedia");
+      setSortOrder(true);
+    } else if (sortBy === "moreold") {
+      setSortBy("moreold");
+      setSortOrder(false);
+    } else if (sortBy === "morenew") {
+      setSortBy("morenew");
+      setSortOrder(true);
+    } else {
+      setSortBy("");
+      setSortOrder(true);
+    }
   };
 
   const filteredRecommendations = recommendations
@@ -29,27 +54,59 @@ export const RecommendationsList = ({ recommendations }) => {
           .includes(categoryQuery.toLowerCase())
       );
     })
-    .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
+    .sort((a, b) => {
+      if (sortBy === "maxmedia") {
+        return sortOrder ? b.media - a.media || 0 : a.media - b.media || 0;
+      } else if (sortBy === "minmedia") {
+        return sortOrder ? a.media - b.media || 0 : b.media - a.media || 0;
+      } else {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      }
+    });
+
+  const totalPages = Math.ceil(
+    filteredRecommendations.length / RECOMMENDATIONS_PER_PAGE
+  );
+
+  const paginatedRecommendations = filteredRecommendations.slice(
+    (currentPage - 1) * RECOMMENDATIONS_PER_PAGE,
+    currentPage * RECOMMENDATIONS_PER_PAGE
+  );
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Buscar por lugar"
-        value={placeQuery}
-        onChange={(e) => setPlaceQuery(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Buscar por categoría"
-        value={categoryQuery}
-        onChange={(e) => setCategoryQuery(e.target.value)}
-      />
-      <button onClick={handleSearch}>Buscar</button>
-      <button onClick={handleClearSearch}>Limpiar</button>
-      {filteredRecommendations.length ? (
+      <div className="sort">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Buscar por lugar"
+            value={placeQuery}
+            onChange={(e) => setPlaceQuery(e.target.value)}
+          />
+          <button onClick={handleSearch}>Buscar</button>
+        </div>
+        <div className="searchRemove">
+          <input
+            type="text"
+            placeholder="Buscar por categoría"
+            value={categoryQuery}
+            onChange={(e) => setCategoryQuery(e.target.value)}
+          />
+          <button onClick={handleClearSearch}>Limpiar</button>
+        </div>
+        <div className="sortMedia">
+          <button onClick={() => handleSortBy("maxmedia")}>
+            Ordenar por mas media
+          </button>
+          <button onClick={() => handleSortBy("minmedia")}>
+            Ordenar por menos media
+          </button>
+        </div>
+      </div>
+      {paginatedRecommendations.length ? (
         <article>
-          {filteredRecommendations.map((recommendation) => (
+          {paginatedRecommendations.map((recommendation) => (
             <ul className="ul" key={recommendation.id}>
               <li>
                 <div className="slide-container">
@@ -58,8 +115,8 @@ export const RecommendationsList = ({ recommendations }) => {
                       <p className="slide-name">{recommendation.user}</p>
                     </Link>
                     <p className="slide-votes">
-                      Nº votos: {recommendation.votes} votos:{" "}
-                      {recommendation.votes}
+                      Nº votos: {recommendation.num_votes} votos:{" "}
+                      {recommendation.media}
                     </p>
                   </div>
 
@@ -87,6 +144,36 @@ export const RecommendationsList = ({ recommendations }) => {
         </article>
       ) : (
         <p>No se encontraron recomendaciones...</p>
+      )}
+      {totalPages > 1 && (
+        <div className="pages">
+          {currentPage > 1 && (
+            <button
+              className="previousNext"
+              onClick={() => setCurrentPage(currentPage - 1)}
+            >
+              Anterior
+            </button>
+          )}
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <button
+              className={`numbers ${page === currentPage ? "active" : ""}`}
+              key={page}
+              onClick={() => setCurrentPage(page)}
+              disabled={page === currentPage}
+            >
+              {page}
+            </button>
+          ))}
+          {currentPage < totalPages && (
+            <button
+              className="previousNext"
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              Siguiente
+            </button>
+          )}
+        </div>
       )}
     </div>
   );
